fc65096b3601f5d4fcea066bd36d3d7f5f029512
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-01-28 14:47:44 +0530

    initial update

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Thankyou to vist on our website we will come soon</h1>
+    <h2>This website is under developed currently for RA Enterprises By Rahul Dhiman @godadyindia</h2>
+</body>
+</html>
