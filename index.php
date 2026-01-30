ce18395a21a6906d73a1554bc106f0efd34ac70e
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-01-30 15:04:16 +0530

    update

--- index.php
+++ index.php
@@ -6,47 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Thankyou to vist on our website we will come soon</h1>
-    <h2>This website is under developed currently for RA Enterprises By Rahul Dhiman @godadyindia</h2>
-    <h3>Thankyou</h3>
-    <h4>RA Enterprises</h4>
-    <h5>Rahul Dhiman</h5>
-    <h6>godadyindia@gmail.com</h6>
-    <h7>godadyindia</h7>
-    <h8>godadyindia</h8>
-    <h9>godadyindia</h9>
-    <h10>godadyindia</h10>
-    <h11>godadyindia</h11>
-    <h12>godadyindia</h12>
-    <h13>godadyindia</h13>
-    <h14>godadyindia</h14>
-    <h15>godadyindia</h15>
+    <h1>RA Enterprises</h1>
 </body>
 </html>
-<style>
-    body {
-        text-align: center;
-        font-family: 'Courier New', Courier, monospace;
-    }
-    h1 {
-        color: red;
-    }
-    h2 {
-        color: green;
-    }
-    h3 {
-        color: blue;
-    }
-    h4 {
-        color: yellow;
-    }
-    h5 {
-        color: orange;
-    }
-    h6 {
-        color: purple;
-    }
-    h7 {
-        color: pink;
-    }
-</style>
